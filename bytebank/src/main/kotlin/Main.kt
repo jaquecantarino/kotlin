abb8c666b95acd13051911b568c4dca405daf34b
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2023-02-12 20:39:04 -0300

    Criando projeto e instanciando variaveis

---
+++ bytebank/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Bem vindo ao ByteBank")
+
+    //atributos
+     val titular = "Zendaya"
+    val numeroConta = 1000
+    var saldo = 0.00
+
+    saldo = 100.00
+    saldo += 10
+    
+    //println("Titular: "+titular) outra forma de fazer isso:
+    println("Titular: $titular")
+    println("Número da Conta: $numeroConta")
+    println("Saldo Atual: $saldo")
+}
